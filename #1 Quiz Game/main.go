8c7aafb3c9c494f9776782b854751149ebc62680
NickRTR <95316598+NickRTR@users.noreply.github.com>
2023-01-21 21:16:42 +0100

    #1: Read CSV file

--- #1 Quiz Game/main.go
+++ #1 Quiz Game/main.go
@@ -1,7 +1,29 @@
 package main
 
-import "fmt"
+import (
+	"encoding/csv"
+	"fmt"
+	"log"
+	"os"
+)
+
+func readCSV() [][]string {
+	f, err := os.Open("problems.csv")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	defer f.Close()
+
+	csvReader := csv.NewReader(f)
+	data, err := csvReader.ReadAll()
+	if err != nil {
+		log.Fatal(err)
+	}
+	return data
+}
 
 func main() {
-	fmt.Println("Hello World!")
+	data := readCSV()
+	fmt.Println(data)
 }
