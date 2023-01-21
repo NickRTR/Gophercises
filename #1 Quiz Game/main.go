2c98b464b98120909048971e3daaac85b9423a25
NickRTR <95316598+NickRTR@users.noreply.github.com>
2023-01-21 22:28:55 +0100

    #1 Part 2: Timer

--- #1 Quiz Game/main.go
+++ #1 Quiz Game/main.go
@@ -6,7 +6,59 @@ import (
 	"fmt"
 	"log"
 	"os"
+	"time"
 )
+
+var file string
+var duration int
+
+func init() {
+	var (
+		defaultFile     = "problems.csv"
+		fileDescription = "Change the path to the quiz CSV, the default is \"problems.csv\""
+	)
+	flag.StringVar(&file, "file", defaultFile, fileDescription)
+	flag.StringVar(&file, "f", file, fileDescription+" (shorthand)")
+
+	var (
+		defaultTimer     = 30
+		timerDescription = "Change the Timer duration, the default is set to 30 seconds"
+	)
+	flag.IntVar(&duration, "timer", defaultTimer, timerDescription)
+	flag.IntVar(&duration, "t", defaultTimer, timerDescription+" (shorthand)")
+
+	flag.Parse()
+}
+
+func main() {
+	correct := 0
+	data := readCSV()
+
+	timer := time.NewTimer(time.Duration(*&duration) * time.Second)
+
+problemLoop:
+	for i, p := range data {
+		fmt.Printf("Problem #%d: %s = ", i+1, p[0])
+		answerCh := make(chan string)
+		go func() {
+			var answer string
+			fmt.Scanf("%s\n", &answer)
+			answerCh <- answer
+		}()
+
+		select {
+		case <-timer.C:
+			fmt.Println()
+			break problemLoop
+		case answer := <-answerCh:
+			if answer == p[1] {
+				correct++
+			}
+		}
+	}
+
+	fmt.Printf("\nResult: %d answers correct, %d answers incorrect.\n", correct, len(data)-correct)
+}
 
 func readCSV() [][]string {
 	f, err := os.Open(file)
@@ -22,48 +74,4 @@ func readCSV() [][]string {
 		log.Fatal(err)
 	}
 	return data
-}
-
-func ask(question, answer string) bool {
-	fmt.Printf("%s:", question)
-	var input string
-	fmt.Scan(&input)
-	return input == answer
-}
-
-var file string
-var timer int
-
-func init() {
-	var (
-		defaultFile     = "problems.csv"
-		fileDescription = "Change the path to the quiz CSV, the default is \"problems.csv\""
-	)
-	flag.StringVar(&file, "file", defaultFile, fileDescription)
-	flag.StringVar(&file, "f", file, fileDescription+" (shorthand)")
-
-	var (
-		defaultTimer     = 30
-		timerDescription = "Change the Timer duration, the default is set to 30 seconds"
-	)
-	flag.IntVar(&timer, "timer", defaultTimer, timerDescription)
-	flag.IntVar(&timer, "t", timer, timerDescription+" (shorthand)")
-
-	flag.Parse()
-}
-
-func main() {
-	data := readCSV()
-
-	correct := 0
-	incorrect := 0
-	for _, quiz := range data {
-		if ask(quiz[0], quiz[1]) {
-			correct++
-		} else {
-			incorrect++
-		}
-	}
-
-	fmt.Printf("\nResult: %d answers correct, %d answers incorrect.\n", correct, incorrect)
 }
